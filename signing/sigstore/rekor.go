9d689cfa0e990c83768ede40a244601870947c1e
Mulugeta Ayalew Tamiru <mulugetayalew@gmail.com>
2023-11-20 10:44:40 +0300

    update rekor url and public key

--- signing/sigstore/rekor.go
+++ signing/sigstore/rekor.go
@@ -27,7 +27,7 @@ import (
 
 const (
 	// DefaultRekorAddr is the default rekor base URL.
-	DefaultRekorAddr = "http://34.18.79.166:3000/"
+	DefaultRekorAddr = "http://34.18.9.50:3000/"
 )
 
 // Rekor implements TransparencyLog.
